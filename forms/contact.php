73a86467d7618511d3879b0467a0107d4f4fe447
deepakraaaj <deepakrajbalasubramanian>
2023-08-29 20:14:35 +0530

    changes done

--- forms/contact.php
+++ forms/contact.php
@@ -1,12 +1,5 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'deepakrajbm.zohomail.in';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -26,10 +19,10 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
   $contact->smtp = array(
-    'host' => 'deepakrajbm@zohomail.in',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'smtp.gmail.com',
+    'username' => '224003173@sastra.ac.in',
+    'password' => 'gzwcpmvyquzuzikq',
+    'port' => '465'
   );
   
 
