dfb9c76586cdbab6d52c742f95dcf55fb8db3973
deepakraaaj <draj39999@gmail.com>
2023-09-11 23:24:05 +0530

    sent

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,24 +2,27 @@
 
 (function () {
     // Initialize EmailJS with your public key
-    emailjs.init('vLMvIcZfAlIaPTY3Y');
+    emailjs.init('YOUR_PUBLIC_KEY');
 })();
 
 window.onload = function () {
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
+    document.getElementById('template_zxax6zb').addEventListener('submit', function (event) {
         event.preventDefault();
         // Generate a five-digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // Use EmailJS to send the form data
         emailjs.sendForm('service_wpe2r72', 'template_zxax6zb', this)
-            .then(function () {
-                console.log('SUCCESS!');
-                // Display a success message to the user
-                document.querySelector('.sent-message').style.display = 'block';
-            }, function (error) {
-                console.log('FAILED...', error);
-                // Display an error message to the user
-                document.querySelector('.error-message').style.display = 'block';
-            });
+        .then(function () {
+            console.log('SUCCESS!');
+            // Hide loading message and show success message
+            document.querySelector('.loading').style.display = 'none';
+            document.querySelector('.sent-message').style.display = 'block';
+        })
+        .catch(function (error) {
+            console.log('FAILED...', error);
+            // Hide loading message and show error message
+            document.querySelector('.loading').style.display = 'none';
+            document.querySelector('.error-message').style.display = 'block';
+        });
     });
 };
