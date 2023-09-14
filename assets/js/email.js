897ed4c8a9b757de5aed634792c9bede46672fc2
deepakraaaj <draj39999@gmail.com>
2023-09-14 14:25:49 +0530

    done

--- assets/js/email.js
+++ assets/js/email.js
@@ -23,45 +23,44 @@ const inputFields = {
 
 // Add an event listener to the contactForm for form submission
 contactForm.addEventListener("submit", function (e) {
-    // Prevent the default form submission behavior
-    e.preventDefault();
-  
-    // Change the button text while processing
-    submitBtn.innerText = "Just A Moment...";
-  
-    // Get all input field values
-    const inputFields = {
-      name: nameInput.value,
-      email: emailInput.value,
-      subject: subjectInput.value, // Include subject input value
-      message: messageInput.value
-    };
-  
-    // Continue with sending the email using EmailJS as shown in the previous code
-    emailjs.send(serviceID, templateID, inputFields)
-      .then(() => {
-        // Change button text after success
-        submitBtn.innerText = "Message Sent Successfully";
-  
-        // Clear out all input fields
-        nameInput.value = '';
-        emailInput.value = '';
-        subjectInput.value = ''; // Clear subject input value
-        messageInput.value = '';
-  
-        // You can also hide or remove any other elements as needed
-        errorMessage.style.display = 'none';
-        loadingMessage.style.display = 'none';
-        sentMessage.style.display = 'block';
-      })
-      .catch((error) => {
-        // Handle any errors that occur during email sending
-        console.error('Email sending failed:', error);
-  
-        // You can also show an error message or take other actions here
-        errorMessage.style.display = 'block';
-        loadingMessage.style.display = 'none';
-        sentMessage.style.display = 'none';
-      });
-  });
-  
+  // Prevent the default form submission behavior
+  e.preventDefault();
+
+  // Change the button text while processing
+  submitBtn.innerText = "Just A Moment...";
+
+  // Get all input field values inside the event listener
+  const inputFields = {
+    name: nameInput.value,
+    email: emailInput.value,
+    subject: subjectInput.value, // Include subject input value
+    message: messageInput.value
+  };
+
+  // Continue with sending the email using EmailJS as shown in the previous code
+  emailjs.send(serviceID, templateID, inputFields)
+    .then(() => {
+      // Change button text after success
+      submitBtn.innerText = "Message Sent Successfully";
+
+      // Clear out all input fields
+      nameInput.value = '';
+      emailInput.value = '';
+      subjectInput.value = ''; // Clear subject input value
+      messageInput.value = '';
+
+      // You can also hide or remove any other elements as needed
+      errorMessage.style.display = 'none';
+      loadingMessage.style.display = 'none';
+      sentMessage.style.display = 'block';
+    })
+    .catch((error) => {
+      // Handle any errors that occur during email sending
+      console.error('Email sending failed:', error);
+
+      // You can also show an error message or take other actions here
+      errorMessage.style.display = 'block';
+      loadingMessage.style.display = 'none';
+      sentMessage.style.display = 'none';
+    });
+});
